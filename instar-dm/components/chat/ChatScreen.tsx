a6d52908cfa1333fea5aa4ed5317099191fa3303
sweet.dh <dlsrksrhk@gmail.com>
2025-05-05 16:48:23 +0900

    Instar DM 개발 완료

---
+++ instar-dm/components/chat/ChatScreen.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { Button, Spinner } from "@material-tailwind/react";
+import Person from "./Person";
+import Message from "./Message";
+import { useRecoilValue } from "recoil";
+import {
+  presenceState,
+  selectedUserIdState,
+  selectedUserIndexState,
+} from "utils/recoil/atoms";
+import { useEffect, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { getAllMessages, getUserById, sendMessage } from "actions/chatActions";
+import { createBrowserSupabaseClient } from "utils/supabase/client";
+
+export default function ChatScreen({}) {
+  const selectedUserId = useRecoilValue(selectedUserIdState);
+  const selectedUserIndex = useRecoilValue(selectedUserIndexState);
+  const [message, setMessage] = useState("");
+  const supabase = createBrowserSupabaseClient();
+  const presence = useRecoilValue(presenceState);
+
+  const selectedUserQuery = useQuery({
+    queryKey: ["user", selectedUserId],
+    queryFn: () => getUserById(selectedUserId),
+  });
+
+  const sendMessageMutation = useMutation({
+    mutationFn: async () => {
+      return sendMessage({
+        message,
+        chatUserId: selectedUserId,
+      });
+    },
+    onSuccess: () => {
+      setMessage("");
+      getAllMessagesQuery.refetch();
+    },
+  });
+
+  const getAllMessagesQuery = useQuery({
+    queryKey: ["messages", selectedUserId],
+    queryFn: () => getAllMessages({ chatUserId: selectedUserId }),
+  });
+
+  useEffect(() => {
+    const channel = supabase
+      .channel("message_postgres_changes")
+      .on(
+        "postgres_changes",
+        {
+          event: "INSERT",
+          schema: "public",
+          table: "message",
+        },
+        (payload) => {
+          //INSERT 이벤트가 발생했을 때, 모든 메시지 조회 API 재 호출
+          if (payload.eventType === "INSERT" && !payload.errors) {
+            getAllMessagesQuery.refetch();
+          }
+        }
+      )
+      .subscribe();
+
+    return () => {
+      //현재 컴포넌트가 unmount될 때 채널 구독 해제
+      channel.unsubscribe();
+    };
+  }, []);
+
+  return selectedUserQuery.data !== null ? (
+    <div className="w-full h-screen flex flex-col">
+      {/* Active 유저 영역 */}
+      <Person
+        index={selectedUserIndex}
+        isActive={false}
+        name={selectedUserQuery.data?.email?.split("@")?.[0]}
+        onChatScreen={true}
+        onlineAt={presence?.[selectedUserId]?.[0]?.onlineAt}
+        userId={selectedUserQuery.data?.id}
+      />
+
+      {/* 채팅 영역 */}
+      <div className="w-full overflow-y-scroll flex-1 flex flex-col p-4 gap-3">
+        {getAllMessagesQuery.data?.map((message) => (
+          <Message
+            key={message.id}
+            message={message.message}
+            isFromMe={message.receiver === selectedUserId}
+          />
+        ))}
+      </div>
+
+      {/* 채팅창 영역 */}
+      <div className="flex">
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="p-3 w-full border-2 border-light-blue-600"
+          placeholder="메시지를 입력하세요."
+        />
+
+        <button
+          onClick={() => sendMessageMutation.mutate()}
+          className="min-w-20 p-3 bg-light-blue-600 text-white"
+          color="light-blue"
+        >
+          {sendMessageMutation.isPending ? <Spinner /> : <span>전송</span>}
+        </button>
+      </div>
+    </div>
+  ) : (
+    <div className="w-full"></div>
+  );
+}
--- instar-dm/utils/recoil/atoms.ts
+++ instar-dm/utils/recoil/atoms.ts
@@ -4,3 +4,23 @@ export const searchState = atom({
   key: "searchState",
   default: "",
 });
+
+export const selectedIndexState = atom({
+  key: "selectedIndexState",
+  default: null,
+});
+
+export const selectedUserIdState = atom({
+  key: "selectedUserIdState",
+  default: null,
+});
+
+export const selectedUserIndexState = atom({
+  key: "selectedUserIndexState",
+  default: null,
+});
+
+export const presenceState = atom({
+  key: "presenceState",
+  default: null,
+});
